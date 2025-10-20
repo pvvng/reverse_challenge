596fa91d9535f3ccd0fbdb5653a04ef8f7fc6200
맥동우 <t6u80o@naver.com>
2025-10-20 14:40:21 +0900

    feat: google search console verification code

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,9 @@ export const metadata: Metadata = {
   title: "리버스 챌린지",
   description:
     "녹음하고 뒤집어 들어봐! 역재생 음성을 따라 역으로 말해 원본과 맞추는 리버스 챌린지",
+  verification: {
+    google: "X8abBNxolxp2vkT2B7w1uCMZm9zkCEGLo0xpbL7Fw9U",
+  },
 };
 
 export default function RootLayout({
